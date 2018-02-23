4d18ec6bad98755f6dd9389ef561f4eb23fd86eb
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-02-23 23:15:49 +0100

    Abilita accesso a MySQL per davvero

--- provision/adminer.sh
+++ provision/adminer.sh
@@ -12,9 +12,3 @@ chown www-data:www-data "$DOCUMENT_ROOT/adminer.php"
 
 echo "Allowing connections from Adminer (root/root)..."
 mysql -uroot -proot -e "USE mysql; UPDATE user SET plugin='' WHERE User='root'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'root' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
-# Comment-out if not needed
-echo "Allowing direct database connections from host to guest..."
-echo "bind-address=0.0.0.0" >> /etc/mysql/my.cnf
-mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION;"
-systemctl restart mysql
--- provision/db.sh
+++ provision/db.sh
@@ -14,10 +14,16 @@ mysql -uroot -proot tarallo < "$DOCUMENT_ROOT/server/database-data.sql"
 echo "Importing sample data..."
 mysql -uroot -proot < "/data/sample-data.sql"
 
-echo "Configuring database..."
+echo "Configuring server to use database..."
 cat << 'EOF' > "$DOCUMENT_ROOT/server/db.php"
 <?php
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DSN', 'mysql:dbname=tarallo;host=localhost;charset=utf8mb4');
 EOF
+
+# Comment-out if not needed
+echo "Allowing direct database connections from host to guest..."
+sed -ie 's#bind-address\s*=\s*127\.0\.0\.1#bind-address=0.0.0.0#g' /etc/mysql/my.cnf
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+systemctl restart mysql
